f7bb403a4bd4b9066ae3f0332437d3563123674f
easy999000 <328178707@qq.com>
2018-12-08 12:31:32 +0800

    添加项目文件。

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace setup
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int userPass = 0;
+            char u1 = '0';
+            while (true)
+            {
+                Console.WriteLine("安装windows服务请输入 1 ,卸载windows服务请输入 2.");
+                userPass = Console.Read();
+
+                  u1 = ((char)userPass);
+                  
+                if (u1 == '1' || u1 == '2')
+                {
+                    break ;
+                }
+            } 
+
+            windowsServer w = new windowsServer();
+            w.run(u1);
+
+
+            //Console.WriteLine("");
+            //Console.WriteLine("");
+            //Console.WriteLine("执行完成,执行结果,请看上面日志.");
+            while (true)
+            {
+                Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ windowsServer.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace setup
+{
+    public class windowsServer
+    {
+        public void run(char i)
+        {
+            string s2 = Environment.Version.ToString();
+             
+            string serversPath1 = "powerStationWindwosServer.exe";
+             
+            if (i== '2')
+            {
+                serversPath1 = "/u " + serversPath1;
+            }
+            string appPath = System.IO.Path.Combine("InstallUtil.exe ");
+            Process pro = new Process();
+            pro.StartInfo.FileName = appPath; 
+
+            pro.StartInfo.Arguments = serversPath1;
+
+            pro.StartInfo.UseShellExecute = false;
+            bool b1 = pro.Start();
+
+        }
+    }
+}
